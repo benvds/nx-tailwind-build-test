245bbc80ae91effb6353c317ed602a02d66706c8
Ben van de Sande <ben@pomodoro.nl>
2021-12-24 10:51:10 +0100

    updates

--- apps/my-app/tailwind.config.js
+++ apps/my-app/tailwind.config.js
@@ -1,9 +1,17 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 
+const nxDeps = createGlobPatternsForDependencies(__dirname); // dirname = apps/my-app
+const content = [
+  ...nxDeps,
+  "./src/**/*.tsx"
+];
+console.log("nxDeps", nxDeps);
+console.log("content", content);
+
 module.exports = {
   purge: {
     enabled: true,
-    content: createGlobPatternsForDependencies(__dirname)
+    content,
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
